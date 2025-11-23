e051c8ccf9c0258275e7e646a21080591e5fc042
Alexey Beloshapkin <Leks11172@mail.ru>
2025-11-23 20:05:33 +0700

    feat: добавлен сервис логирования и обработка команд CLI
    - Добавлен сервис log.service.js с цветным выводом сообщений
    - Реализованы функции printError, printSuccess и printHelp
    - Добавлена обработка флагов -h (справка) и -s (сохранение города)
    - Интегрированы библиотеки chalk и dedent-js для форматирования вывода
    - Добавлен .gitignore для исключения node_modules

---
+++ services/log.service.js
@@ -0,0 +1,21 @@
+import chalk from "chalk";
+import dedent from "dedent-js";
+
+const printError = (error) => {
+  console.log(chalk.bgRed(" ERROR "), " ", error);
+};
+const printSuccess = (success) => {
+  console.log(chalk.bgGreen(" SUCCESS "), " ", success);
+};
+
+const printHelp = () => {
+  console.log(dedent`
+		${chalk.bgCyan(" HELP ")}
+		Без параметров - вывод погоды
+		-s [CITY] для установки города
+		-h для вывода помощи
+		-t [API_KEY] сохранения токена
+		`);
+};
+
+export { printError, printSuccess, printHelp };
--- weather.js
+++ weather.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
+import { printError, printHelp, printSuccess } from "./services/log.service.js";
 
 const initCLI = () => {
   const args = getArgs(process.argv);
-  console.log(args);
+  if (args.h) {
+    printHelp();
+  }
+  if (args.s) {
+    printSuccess(`Город сохранен: ${args.s}`);
+  }
+  printError("Что то пошло не так");
 };
 initCLI();
